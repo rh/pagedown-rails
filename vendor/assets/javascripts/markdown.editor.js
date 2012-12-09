891b6b7d9cfc02770a975c3baee5b6deb7bd11bc
Les Peabody <lpeabody@gmail.com>
2012-12-09 18:37:22 -0500

    Modified markdown.editor.js PanelCollection function to grab the input element from the DOM based on class name rather than ID, so it doesn't interfere with Rails ID-centric parameter passing.

--- vendor/assets/javascripts/markdown.editor.js
+++ vendor/assets/javascripts/markdown.editor.js
@@ -249,7 +249,8 @@
     function PanelCollection(postfix) {
         this.buttonBar = doc.getElementById("wmd-button-bar" + postfix);
         this.preview = doc.getElementById("wmd-preview" + postfix);
-        this.input = doc.getElementById("wmd-input" + postfix);
+        // this.input = doc.getElementById("wmd-input" + postfix);
+        this.input = doc.getElementsByClassName("wmd-input" + postfix)[0];
     };
 
     // Returns true if the DOM element is visible, false if it's hidden.
